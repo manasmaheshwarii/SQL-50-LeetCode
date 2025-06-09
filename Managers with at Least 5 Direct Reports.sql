0dee4633ef74b3dcedc2f1b47f80bce9c03c7207
Manas Maheshwari <130285390+manasmaheshwarii@users.noreply.github.com>
2025-06-09 23:58:01 +0530

    Update Managers with at Least 5 Direct Reports.sql

--- Managers with at Least 5 Direct Reports.sql
+++ Managers with at Least 5 Direct Reports.sql
@@ -6,11 +6,3 @@ WHERE id IN
    GROUP BY managerId 
    HAVING COUNT(*) >= 5
   )
-
--- OR
-SELECT a.name
-FROM Employee a
-JOIN Employee b
-WHERE a.id = b.managerId
-GROUP BY b.managerId
-HAVING COUNT(*) >= 5
